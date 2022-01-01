12724f7ddb5adcfedec84c72563de945f24e9c67
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2022-01-01 06:22:53 -0300

    refactor: add new gitignore & fix execute script

--- execute.sh
+++ execute.sh
@@ -2,12 +2,8 @@
 
 read -p "Enter a script to execute (array, stack, linked_list...): " FOLDER_NAME
 
-if
-  [ $FOLDER_NAME == "array" ] ||
-  [ $FOLDER_NAME == "linked_list" ] ||
-  [ $FOLDER_NAME == "stack" ];
-then
-  gcc -o ./$FOLDER_NAME/main ./$FOLDER_NAME/main.c && ./$FOLDER_NAME/main
+if [ "$FOLDER_NAME" = "array" ] || [ "$FOLDER_NAME" = "linked_list" ] || [ "$FOLDER_NAME" = "stack" ]; then
+  gcc -o ./$FOLDER_NAME/main.o ./$FOLDER_NAME/main.c && ./$FOLDER_NAME/main.o
 else
   echo "Data structure not found, try again"
 fi
